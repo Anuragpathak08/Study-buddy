1d8e13e49abfa2e3ffbcc7ff9545f6fd5c99cafc
Anuragpathak08 <anuragasp123@gmail.com>
2025-12-05 22:33:19 +0530

    chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    user1  = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    user2 = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
