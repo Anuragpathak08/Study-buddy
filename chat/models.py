9b7308e536de6e6194e6d0432f1108ee42902e2b
Anuragpathak08 <anuragasp123@gmail.com>
2025-12-07 22:55:29 +0530

    readme update

--- chat/models.py
+++ chat/models.py
@@ -6,3 +6,6 @@ class Conversation(models.Model):
     name = models.TextField(max_length=255, blank=True, null=True)
     isgroup = models.models.BooleanField(default = True)
     participant = models.ManyToManyField( settings.AUTH_MODEL_USER)
+
+class Chat(models.Model):
+    conv = models.ForeignKey(Conversation , on_delete=Cas)
